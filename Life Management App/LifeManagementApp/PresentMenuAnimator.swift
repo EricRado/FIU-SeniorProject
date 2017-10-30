665bfdcea398fdcceee1e610f5a64de8d21ec106
Eric Rado <ericvictor14@hotmail.com>
2017-10-30 10:23:18 -0400

    Added code for MenuHelper and Interactor
    Helper functions for side menu implementation

---
+++ Life Management App/LifeManagementApp/PresentMenuAnimator.swift
@@ -0,0 +1,9 @@
+//
+//  PresentMenuAnimator.swift
+//  LifeManagementApp
+//
+//  Created by Eric Rado on 10/30/17.
+//  Copyright © 2017 SeniorProject. All rights reserved.
+//
+
+import Foundation
