9622c781186b7c406b512204dd6bd0f0c41413ca
Eric Rado <ericvictor14@hotmail.com>
2017-10-30 10:41:48 -0400

    Added dismiss side menu animation

--- Life Management App/LifeManagementApp/DismissMenuAnimator.swift
+++ Life Management App/LifeManagementApp/DismissMenuAnimator.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import UIKit
+
+class DismissMenuAnimator: NSObject {
+    
+}
+
+extension DismissMenuAnimator: UIViewControllerAnimatedTransitioning{
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
+        let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
+        let containerView = transitionContext.containerView
+        
+        // get a handle of the snapshot view
+        let snapshot = containerView.viewWithTag(MenuHelper.snapshotNumber)
+        
+        UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+                // the animation moves the snapshot back to the center of the screen
+            snapshot?.frame = CGRect(origin: CGPoint.zero, size: UIScreen.main.bounds.size)
+        }, completion: { _ in
+            let didTransitionComplete = !transitionContext.transitionWasCancelled
+            
+            if didTransitionComplete {
+                // if animation finishes, replace the snapshot with the real thing
+                containerView.insertSubview(toVC!.view, aboveSubview: fromVC!.view)
+                snapshot?.removeFromSuperview()
+            }
+            transitionContext.completeTransition(didTransitionComplete)
+        })
+
+    }
+}
