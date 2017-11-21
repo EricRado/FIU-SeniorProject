bc1cba468d2da27a4d37513d6d59a9c52feebbfa
Eric Rado <ericvictor14@hotmail.com>
2017-11-21 17:54:15 -0500

    Added drop down menu with gestures

--- Life Management App/LifeManagementApp/CurrentChatsVC.swift
+++ Life Management App/LifeManagementApp/CurrentChatsVC.swift
@@ -7,13 +7,73 @@
 //
 
 import UIKit
+import Firebase
+
+extension CurrentChatsVC: UIViewControllerTransitioningDelegate{
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return PresentMenuAnimator()
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return DismissMenuAnimator()
+    }
+    
+    /* indicate that the dismiss transition is going to be interactive, but
+     only if the user is panning */
+    
+    func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactor.hasStarted ? interactor : nil
+    }
+    
+    func interactionControllerForPresentation(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return interactor.hasStarted ? interactor : nil
+    }
+}
 
 class CurrentChatsVC: UIViewController {
+    
+    let interactor = Interactor()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
+    
+    /***********************************************************
+     
+                        Side Menu Functions
+     
+     ***********************************************************/
+    
+    
+    @IBAction func openMenu(_ sender: AnyObject){
+        performSegue(withIdentifier: "openMenu", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destinationViewController = segue.destination as? SideMenuViewController{
+            destinationViewController.transitioningDelegate = self
+            // pass the interactor object forward
+            destinationViewController.interactor = interactor
+        }
+    }
+    
+    @IBAction func edgePanGesture(sender: UIScreenEdgePanGestureRecognizer){
+        let translation = sender.translation(in: view)
+        
+        let progress = MenuHelper.calculateProgress(translationInView: translation, viewBounds: view.bounds, direction: .Right)
+        
+        MenuHelper.mapGestureStateToInteractor(gestureState: sender.state, progress: progress, interactor: interactor){
+            self.performSegue(withIdentifier: "openMenu", sender: nil)
+        }
+    }
+
+    
 
 }
+
+
+
+
+
