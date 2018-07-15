da3ce54cadfda390f54e110e0d8ea29e81ebdbf1
Eric Rado <eric@aspforce.com>
2018-07-15 15:09:44 -0400

    Scrolling fixed for emotionsVC

---
+++ Life Management App/LifeManagementApp/EmotionVC.swift
@@ -0,0 +1,36 @@
+//
+//  EmotionVC.swift
+//  LifeManagementApp
+//
+//  Created by Eric Rado on 7/15/18.
+//  Copyright © 2018 SeniorProject. All rights reserved.
+//
+
+import UIKit
+
+class EmotionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.layoutIfNeeded()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
