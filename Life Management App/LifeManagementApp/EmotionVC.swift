a6cae41bf0c85d29834946a19ea95190745d78bb
Eric Rado <eric@aspforce.com>
2018-07-15 22:32:52 -0400

    added ui outlets to emotion vc
    emotion VC is going to be a super class to the three emotions the app will track

--- Life Management App/LifeManagementApp/EmotionVC.swift
+++ Life Management App/LifeManagementApp/EmotionVC.swift
@@ -9,28 +9,44 @@
 import UIKit
 
 class EmotionVC: UIViewController {
-
+    
+    // :- MARK IBOutlet Instances
+    @IBOutlet weak var startDateLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    
+    @IBOutlet weak var a1GoalScoreLabel: UILabel!
+    @IBOutlet weak var a1ActualScoreLabel: UILabel!
+    @IBOutlet weak var a1ScorePercentageLabel: UILabel!
+    
+    @IBOutlet weak var a2GoalScoreLabel: UILabel!
+    @IBOutlet weak var a2ActualScoreLabel: UILabel!
+    @IBOutlet weak var a2ScorePercentageLabel: UILabel!
+    
+    
+    @IBOutlet weak var a1Image: UIImageView!
+    @IBOutlet weak var a2Image: UIImageView!
+    
+    @IBOutlet weak var emotionLabel: UILabel!
+    
+    @IBOutlet weak var emotionScorePercentageLabel: UILabel!
+    @IBOutlet weak var overallScorePercentageLabel: UILabel!
+    
+    @IBOutlet weak var emotionScore: KDCircularProgress!
+    @IBOutlet weak var overallScore: KDCircularProgress!
+    
+    
+    @IBOutlet weak var question1TextField: UITextField!
+    @IBOutlet weak var question2TextField: UITextField!
+    @IBOutlet weak var question3TextField: UITextField!
+    @IBOutlet weak var question4TextField: UITextField!
+    
+    @IBOutlet weak var submitBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.layoutIfNeeded()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
